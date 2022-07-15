791a5aaa97c2b01885abc3aacdbee09a8a90ec5e
Niño James A. Tan <njtan142@gmail.com>
2022-07-15 17:03:41 +0800

    Pieces are now moveable but there's no gameplay mechanics yet

--- lib/main.dart
+++ lib/main.dart
@@ -39,8 +39,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              width: MediaQuery.of(context).size.width * 0.90,
-              height: MediaQuery.of(context).size.width * 0.90,
+              width: MediaQuery.of(context).size.height * 0.70,
+              height: MediaQuery.of(context).size.height * 0.70,
               child: CheckerBoard(),
             )
           ],
@@ -63,11 +63,18 @@ var cell = [
 ];
 
 // a enum whether it is player 1 or player 2
-enum Player { player1, player2, empty }
+enum Player { player1, player2, empty, outOfBounds }
 
 enum Turn { player1, player2 }
 
-late Turn turn;
+enum GameState {
+  currentTurn,
+  nextTurn,
+}
+
+Turn turn = Turn.player1;
+
+GameState gameState = GameState.nextTurn;
 
 void putPiece(int row, int col, Player player) {
   if (player == Player.player1) {
@@ -75,6 +82,7 @@ void putPiece(int row, int col, Player player) {
   } else {
     cell[row][col] = 2;
   }
+  print(cell);
 }
 
 void initializeGame() {
@@ -92,8 +100,61 @@ void initializeGame() {
 }
 
 //make a gameboard with 8 rows and 8 columns and alternative colors
+var pieces = <CheckerPiece>[];
 
-class CheckerPiece extends StatelessWidget {
+// a widget array with 8 rows and 8 columns
+class CheckerBoard extends StatelessWidget {
+  CheckerBoard({Key? key}) : super(key: key);
+
+  void putPieces() {
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        if (i < 3) {
+          pieces.add(CheckerPiece(
+            hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
+            row: i,
+            column: j,
+            player: (i + j) % 2 == 1 ? Player.player1 : Player.outOfBounds,
+          ));
+        } else if (i > 4) {
+          pieces.add(CheckerPiece(
+            hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
+            row: i,
+            column: j,
+            player: (i + j) % 2 == 1 ? Player.player2 : Player.outOfBounds,
+          ));
+        } else {
+          pieces.add(CheckerPiece(
+            hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
+            row: i,
+            column: j,
+            player: (i + j) % 2 == 1 ? Player.empty : Player.outOfBounds,
+          ));
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    putPieces();
+    return GridView.count(
+      crossAxisCount: 8,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: pieces,
+    );
+  }
+}
+
+class CheckerPiece extends StatefulWidget {
+  Color hue;
+
+  final int row;
+  final int column;
+
+  Player player;
+
   CheckerPiece(
       {Key? key,
       required this.hue,
@@ -102,20 +163,21 @@ class CheckerPiece extends StatelessWidget {
       required this.player})
       : super(key: key);
 
-  final Color hue;
+  @override
+  State<CheckerPiece> createState() => _CheckerpieceState();
+  void setPiece() {
+    _CheckerpieceState()._setPiece();
+  }
+}
 
-  final row;
-  final column;
-
-  Player player;
-
+class _CheckerpieceState extends State<CheckerPiece> {
   var size;
 
   Text _checkplayer() {
-    if (player == Player.player1 && hue == Colors.black) {
+    if (widget.player == Player.player1 && widget.hue == Colors.black) {
       return Text("⬤",
           style: TextStyle(color: Colors.red, fontSize: size * 0.0625));
-    } else if (player == Player.player2 && hue == Colors.black) {
+    } else if (widget.player == Player.player2 && widget.hue == Colors.black) {
       return Text("⬤",
           style: TextStyle(color: Colors.blue, fontSize: size * 0.0625));
     } else {
@@ -123,54 +185,68 @@ class CheckerPiece extends StatelessWidget {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    size = MediaQuery.of(context).size.width;
-    return Container(
-      color: hue,
-      child: Center(
-        child: _checkplayer(),
-      ),
-    );
+  Text piece = Text("");
+
+  void _setPiece() {
+    setState(() {
+      piece = _checkplayer();
+    });
   }
-}
-
-// a widget array with 8 rows and 8 columns
-class CheckerBoard extends StatelessWidget {
-  const CheckerBoard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 8,
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      children: <Widget>[
-        for (int i = 0; i < 8; i++)
-          for (int j = 0; j < 8; j++)
-            // row 1 to 3 are player 1, row 6 to 8 are player 2, the rest are empty
-            if (i < 3)
-              CheckerPiece(
-                hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
-                row: i,
-                column: j,
-                player: Player.player1,
-              )
-            else if (i > 4)
-              CheckerPiece(
-                hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
-                row: i,
-                column: j,
-                player: Player.player2,
-              )
-            else
-              CheckerPiece(
-                hue: (i + j) % 2 == 1 ? Colors.black : Colors.white,
-                row: i,
-                column: j,
-                player: Player.empty,
-              )
-      ],
+    if (MediaQuery.of(context).size.height >
+        MediaQuery.of(context).size.width) {
+      size = MediaQuery.of(context).size.width;
+    } else {
+      size = MediaQuery.of(context).size.height;
+    }
+    _setPiece();
+    return GestureDetector(
+      onTap: () {
+        print(widget.player);
+        if (turn == Turn.player1) {
+          if (widget.player == Player.player1) {
+            putPiece(widget.row, widget.column, Player.player1);
+            if (gameState == GameState.nextTurn) {
+              gameState = GameState.currentTurn;
+              widget.player = Player.empty;
+              _setPiece();
+            }
+          } else {
+            print(gameState);
+            if (gameState == GameState.currentTurn) {
+              gameState = GameState.nextTurn;
+              widget.player = Player.player1;
+              _setPiece();
+              turn = Turn.player2;
+            }
+          }
+        } else {
+          if (widget.player == Player.player2) {
+            putPiece(widget.row, widget.column, Player.player2);
+            if (gameState == GameState.nextTurn) {
+              gameState = GameState.currentTurn;
+              widget.player = Player.empty;
+              _setPiece();
+            }
+          } else {
+            print(gameState);
+            if (gameState == GameState.currentTurn) {
+              gameState = GameState.nextTurn;
+              widget.player = Player.player2;
+              _setPiece();
+              turn = Turn.player1;
+            }
+          }
+        }
+      },
+      child: Container(
+        color: widget.hue,
+        child: Center(
+          child: piece,
+        ),
+      ),
     );
   }
 }
